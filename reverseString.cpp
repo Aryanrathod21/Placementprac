a4e17a4d6a4101fc55f90906c292032d385080b5
Aryanrathod21 <aryanrathod410@gmail.com>
2024-07-12 19:43:29 +0530

    reverse string

---
+++ reverseString.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void reverseString(string &str) {
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++) {
+        swap(str[i], str[n - i - 1]);
+    }
+}
+
+int main() {
+    string str = "Aryan";
+    reverseString(str);
+    cout << str << endl;
+    return 0;
+}
