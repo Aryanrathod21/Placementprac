239d6dc71cb3a89a5c16618f89daf4abb0ba477e
Aryanrathod21 <aryanrathod410@gmail.com>
2024-07-13 18:50:45 +0530

    hollow pattern

---
+++ patternshollow.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rows,cols;
+    cout<<"Enter the numbers of rows : ";
+    cin>>rows;
+    cout<<"Enter the numbers of cols : ";
+    cin>>cols;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= cols; j++)
+        {
+            if ( i==1 || i==rows)
+            {
+                cout<<"*";
+            }
+            else if ( j==1 || j==cols)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+            
+            
+            
+        }
+        cout<<endl;
+        
+    }
+    
+
+}
